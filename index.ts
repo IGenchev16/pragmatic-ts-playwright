4ff3e7a75ca5c24d894397eb5103403144f4f29f
Yordan Genchev <igenchev16@gmail.com>
2025-10-13 21:05:11 +0300

    homework typescript

--- index.ts
+++ index.ts
@@ -1,2 +1,22 @@
-console.log('sadisad');
-console.log('123124124124');
+// Task 1
+const name: string = "Petar Ivanov"
+const age: number = 20
+
+console.log('Hello! My name is ' +name + ', ' +age + ' years old');
+
+// Task 2
+const num1: number = 2
+const num2: number = 4
+const add: number = num1 + num2
+const subs: number = num2 - num1
+console.log(add);
+console.log(subs);
+
+//Task3
+const numbers = [66, 12];
+
+numbers.forEach(num => {
+  const isWithinRange = num >= 10 && num <= 50;
+  console.log(`Is number ${num} within range? : ${isWithinRange}`);
+});
+
