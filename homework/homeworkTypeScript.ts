3f541747bbffb55174127b715d745dfdbd75915f
Yordan Genchev <igenchev16@gmail.com>
2025-10-14 19:37:50 +0300

    adding packages

--- homework/homeworkTypeScript.ts
+++ homework/homeworkTypeScript.ts
@@ -1,8 +1,8 @@
 // Task 1
-const name: string = "Petar Ivanov"
-const age: number = 20
+const name: string = 'Petar Ivanov';
+const age: number = 20;
 
-console.log('Hello! My name is ' +name + ', ' +age + ' years old');
+console.log('Hello! My name is ' + name + ', ' + age + ' years old');
 
 // Task 2
 const [num1, num2] = [2, 4];
@@ -12,8 +12,7 @@ console.log(`${num2} - ${num1} = `, num2 - num1);
 //Task3
 const numbers = [66, 12];
 
-numbers.forEach(num => {
+numbers.forEach((num) => {
   const isWithinRange = num >= 10 && num <= 50;
   console.log(`Is number ${num} within range? : ${isWithinRange}`);
 });
-
